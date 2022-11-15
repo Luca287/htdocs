f596714c3271ead492563129ab0ff645808836d9
Luca <luca.pinotti777@gmail.com>
2022-11-15 17:39:10 -0300

    luca

--- final/script.js
+++ final/script.js
@@ -1,12 +1,12 @@
-var total = 0
 var peticion = new XMLHttpRequest();
 peticion.open("GET", "./productos.json", true);
 
 peticion.addEventListener("readystatechange", function () {
     if (this.readyState == 4 && this.status == 200) {
         var productos = JSON.parse(this.responseText);
-         console.log(productos);
+        console.log(productos);
     }
 
-    
+
 });
+
