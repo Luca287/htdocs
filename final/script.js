ab59863d1c4c78145bcb2eb979727b7dc659c456
Luca <luca.pinotti777@gmail.com>
2022-11-16 19:12:25 -0300

    confirmar

--- final/script.js
+++ final/script.js
@@ -6,38 +6,38 @@ peticion.open("GET", "./productos.json", true);
 peticion.addEventListener("readystatechange", function () {
     if (this.readyState == 4 && this.status == 200) {
         var productos = JSON.parse(this.responseText);
-        
-       productos.forEach(element => {
-         var folletos = document.getElementById("productos")
-         var div = document.createElement ("div");
-         div.appendChild (folletos)
 
-         var link = document.createElement('a');
-         link.setAttribute('href', '#');
-         link.innerText = productos.nombre;
+        productos.forEach(element => {
+            var folletos = document.getElementById("productos")
+            var div = document.createElement("div");
+            div.appendChild(folletos)
 
-         div.appendChild (link);
+            var link = document.createElement('a');
+            link.setAttribute('href', '#');
+            link.innerText = productos.nombre;
 
-        var imagen = document.createElement ('img');
-        imagen.setAttribute ('alt', "imagen del producto");
-        imagen.setAttribute ('src', productos.url_foto);  
+            div.appendChild(link);
 
-        div.appendChild (imagen);
-            
-        var descripcion = document.createElement ("p");
-        descripcion.innerText = (productos.descripcion);
+            var imagen = document.createElement('img');
+            imagen.setAttribute('alt', "imagen del producto");
+            imagen.setAttribute('src', productos.url_foto);
 
-        div.appendChild (descripcion)
+            div.appendChild(imagen);
 
-            });
+            var descripcion = document.createElement("p");
+            descripcion.innerText = (productos.descripcion);
 
-        
-      
+            div.appendChild(descripcion)
+
+        });
 
 
 
 
-         
+
+
+
+
     }
 });
 peticion.send();
