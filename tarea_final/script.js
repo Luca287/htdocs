9efba20a205f0da73e0aef5df53cf5938f851ba7
Luca <luca.pinotti777@gmail.com>
2022-11-17 01:27:30 -0300

    confirmar

--- tarea_final/script.js
+++ tarea_final/script.js
@@ -5,7 +5,7 @@ peticion.open("GET", "./productos.json", true);
 
 peticion.addEventListener("readystatechange", function () {
     if (this.readyState == 4 && this.status == 200) {
-        var productos = JSON.parse(this.responseText);
+        var productos = JSON.parse(peticion.responseText);
 
         productos.forEach(productos => {
 
