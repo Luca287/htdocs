35003861b036f94ee51d927a723d05162adec07f
Luca <luca.pinotti777@gmail.com>
2022-11-18 11:29:02 -0300

    confirmar

--- tarea_final/script.js
+++ tarea_final/script.js
@@ -72,7 +72,7 @@ peticion.addEventListener("readystatechange", function () {
                     event.preventDefault();
                     console.log(event.target.parentElement.parentElement.remove());
 
-                    pTotal = total - productos.precio;
+                    total = total - productos.precio;
                     contador = contador - 1
                     pcontador.innerText = contador
                     ptotal.innerText = total
